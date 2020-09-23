0221e6942f7aa2eab693acfd9ca8309ac9ab1b15
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-09-23 13:11:07 +0200

    Add first test

--- e2e/questionList_test.js
+++ e2e/questionList_test.js
@@ -1,5 +1,7 @@
 Feature('questionList');
 
-Scenario('test something', (I) => {
-
+Scenario('Test that the questions are shown', (I) => {
+  I.amOnPage("http://localhost:8080/mvc-simple/questions");
+  I.see("My first title");
+  I.see("Something else");
 });
