0798621dab68e0ee3403555ecbf0235386c30d2c
Alexandre Piveteau <alexandre@piveteau.email>
2020-09-24 19:11:31 +0200

    Deploy mvc-simple to the root of the URL

--- docker/images/payara/entrypoint.sh
+++ docker/images/payara/entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-java -Xmx175m -XX:+UseContainerSupport -XX:MaxRAMPercentage=90.0 -jar payara-micro.jar --deploymentDir /opt/payara/deployments --port $PORT
+java                                                    \
+    -Xmx175m                                            \
+    -XX:+UseContainerSupport                            \
+    -XX:MaxRAMPercentage=90.0                           \
+    -jar payara-micro.jar                               \
+    --deploy /opt/payara/deployments/mvc-simple.war:    \
+    --port $PORT
