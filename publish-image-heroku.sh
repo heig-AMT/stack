16c9e4574c795d3bfbd27072383eb7c1fc3b81c8
Alexandre Piveteau <alexandre@piveteau.email>
2020-09-24 13:23:45 +0200

    Try to fix TTY login to heroku

--- publish-image-heroku.sh
+++ publish-image-heroku.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 docker tag stack-payara registry.heroku.com/heig-amt-stackunderflow/web
-echo $TOKEN_HEROKU | docker login --username=_ --password-stdin registry.heroku.com
+echo $TOKEN_HEROKU | docker login registry.heroku.com -u _ --password-stdin
 docker push registry.heroku.com/heig-amt-stackunderflow/web
