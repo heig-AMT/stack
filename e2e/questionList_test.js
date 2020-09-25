ac12c89ceca753c8c99f95eb8194587c710e44c3
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-09-25 07:52:49 +0200

    Fix questionList test to reflect new page

--- e2e/questionList_test.js
+++ e2e/questionList_test.js
@@ -2,6 +2,12 @@ Feature('questionList');
 
 Scenario('Test that the questions are shown', (I) => {
   I.amOnPage("http://localhost:8080/mvc-simple/questions");
-  I.see("My first title");
-  I.see("Something else");
+
+  // Check that we have table headers
+  I.see("Title");
+  I.see("Description");
+
+  // Check that we have pre-made questions, for now
+  I.see("Why doesn't this program run ?");
+  I.see("Can we inject an EJB in a JSP ?");
 });
