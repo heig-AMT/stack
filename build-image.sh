fc81ef141c0ff31d8b26d63eacc048c097a3adee
Alexandre Piveteau <alexandre@piveteau.email>
2020-09-29 11:14:52 +0200

    Use different server.xml configuration files for Maven and Docker workflows

--- build-image.sh
+++ build-image.sh
@@ -12,5 +12,4 @@ docker build -t stack-payara ./docker/images/payara
 # Open Liberty
 echo "Building open-liberty Docker image"
 cp target/mvc-simple.war docker/images/openliberty
-cp src/main/liberty/config/server.xml docker/images/openliberty
 docker build -t stack-openliberty ./docker/images/openliberty
