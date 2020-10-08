fecd4a2f7630392e8ca40770bb6c1c2aa46f00a0
Alexandre Piveteau <alexandre@piveteau.email>
2020-10-08 23:14:08 +0200

    Make miscellaneous improvements

--- src/main/java/ch/heigvd/amt/stack/infrastructure/persistence/database/JdbcCredentialRepository.java
+++ src/main/java/ch/heigvd/amt/stack/infrastructure/persistence/database/JdbcCredentialRepository.java
@@ -1,18 +1,20 @@
 package ch.heigvd.amt.stack.infrastructure.persistence.database;
 
 import ch.heigvd.amt.stack.application.authentication.query.CredentialQuery;
+import ch.heigvd.amt.stack.domain.authentication.AuthenticationFailedException;
 import ch.heigvd.amt.stack.domain.authentication.Credential;
 import ch.heigvd.amt.stack.domain.authentication.CredentialId;
 import ch.heigvd.amt.stack.domain.authentication.CredentialRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Alternative;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 @Alternative
@@ -20,58 +22,69 @@ public class JdbcCredentialRepository extends JdbcRepository<Credential, Credent
 
     @Override
     public Optional<Credential> findBy(CredentialQuery query) {
-        return Optional.empty();
+        // TODO : Offer an optimized implementation, with SQL-specific optimizations.
+        return findAll().stream()
+                .filter(cred -> cred.getUsername().equals(query.getUsername()))
+                .findAny();
     }
 
     @Override
     public void save(Credential credential) {
+        var insert = "INSERT INTO Credential(idCredential, username, hash) VALUES (?, ?, ?);";
         try {
-            PreparedStatement ps = getDataSource().getConnection().prepareStatement(
-                    "INSERT INTO Credential VALUES (" + credential.getId().toString() + ","
-                            + credential.getUsername() + "," + credential.getHashedPassword() + ");");
-            ps.execute();
+            var statement = getDataSource().getConnection().prepareStatement(insert);
+            statement.setString(1, credential.getId().toString());
+            statement.setString(2, credential.getUsername());
+            statement.setString(3, credential.getHashedPassword());
+            statement.execute();
         } catch (SQLException ex) {
+            // TODO : Make sure that duplicates are properly handed.
+            throw new AuthenticationFailedException();
         }
     }
 
     @Override
     public void remove(CredentialId credentialId) {
+        var delete = "DELETE FROM Credential WHERE idCredential = ?;";
         try {
-            PreparedStatement ps = getDataSource().getConnection().prepareStatement(
-                    "DELETE FROM Credential WHERE idCredential=\'" + credentialId.toString() + "\'))");
-            ps.execute();
+            var statement = getDataSource().getConnection().prepareStatement(delete);
+            statement.setString(1, credentialId.toString());
+            statement.execute();
         } catch (SQLException ex) {
+            Logger.getLogger("JDBC").log(Level.WARNING, "Could not remove credential " + credentialId);
         }
     }
 
     @Override
     public Optional<Credential> findById(CredentialId credentialId) {
-        Optional<Credential> result = Optional.empty();
+        var select = "SELECT * FROM Credential WHERE idCredential = ?;";
         try {
-            PreparedStatement ps = getDataSource().getConnection().prepareStatement(
-                    "SELECT * FROM Credential WHERE idCredential=\'" + credentialId.toString() + "\'))");
-            ResultSet rs = ps.executeQuery();
+            var statement = getDataSource().getConnection().prepareStatement(select);
+            statement.setString(1, credentialId.toString());
+            var rs = statement.executeQuery();
 
-            while (rs.next()) {
-                Credential credential = Credential.builder()
+            if (rs.next()) {
+                return Optional.of(Credential.builder()
                         .id(CredentialId.from(rs.getString("idCredential")))
                         .username(rs.getString("username"))
-                        .hashedPassword(rs.getString("hash")).build();
-                result = Optional.of(credential);
+                        .hashedPassword(rs.getString("hash")).build());
+            } else {
+                return Optional.empty();
             }
+
         } catch (SQLException ex) {
+            Logger.getLogger("JDBC").log(Level.WARNING, "Could not find credential " + credentialId);
         }
-        return result;
+        return Optional.empty();
     }
 
     @Override
     public Collection<Credential> findAll() {
-        ArrayList<Credential> result = new ArrayList<>();
+        var select = "SELECT * FROM Credential;";
+        Collection<Credential> result = new ArrayList<>();
         try {
-            PreparedStatement ps = getDataSource().getConnection().prepareStatement(
-                    "SELECT * FROM Credential;");
-            ResultSet rs = ps.executeQuery();
-
+            var statement = getDataSource().getConnection().prepareStatement(select);
+            var rs = statement.executeQuery();
             while (rs.next()) {
                 Credential credential = Credential.builder()
                         .id(CredentialId.from(rs.getString("idCredential")))
@@ -80,6 +93,8 @@ public class JdbcCredentialRepository extends JdbcRepository<Credential, Credent
                 result.add(credential);
             }
         } catch (SQLException ex) {
+            Logger.getLogger("JDBC").log(Level.WARNING, "Could not findAll()");
+            return Collections.emptyList();
         }
         return result;
     }
