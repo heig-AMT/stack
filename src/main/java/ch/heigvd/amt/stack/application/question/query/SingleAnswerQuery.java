054fcea59641abea7b4313867395990504368f40
Alexandre Piveteau <alexandre@piveteau.email>
2020-10-22 14:32:32 +0200

    Add SingleAnswerQuery to QuestionFacade

---
+++ src/main/java/ch/heigvd/amt/stack/application/question/query/SingleAnswerQuery.java
@@ -0,0 +1,11 @@
+package ch.heigvd.amt.stack.application.question.query;
+
+import ch.heigvd.amt.stack.domain.answer.AnswerId;
+import lombok.Builder;
+import lombok.Value;
+
+@Builder
+@Value
+public class SingleAnswerQuery {
+    AnswerId id;
+}
