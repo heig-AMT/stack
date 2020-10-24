c14f2137661f4ad3269ebd00fcccf506d06fe6e5
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-10-24 10:23:58 +0200

    Add test: multiple people can vote

--- tests/e2e/tests/votes_test.js
+++ tests/e2e/tests/votes_test.js
@@ -93,3 +93,59 @@ Scenario('Cannot vote when logged out', async (I, registerPage, logoutPage, ques
   I.dontSeeElement({css: '.voteBox:nth-of-type(1) form:nth-of-type(1) input[type="image"]'});
   I.dontSeeElement({css: '.voteBox:nth-of-type(1) form:nth-of-type(2) input[type="image"]'});
 });
+
+Scenario('Multiple people can vote', async (I, logoutPage, registerPage, questionsPage, answersPage) => {
+  let numUpvotes = 0;
+  let numDownvotes = 0;
+
+  registerPage.register();
+  const question = questionsPage.addQuestion();
+  I.click("Questions")
+  I.click(question.title);
+
+  answersPage.addAnswer();
+
+  numUpvotes = await I.grabTextFrom('.voteBox:nth-of-type(1) span:nth-of-type(1)')
+  numDownvotes = await I.grabTextFrom('.voteBox:nth-of-type(1) span:nth-of-type(2)')
+
+  assert.equal(0, numUpvotes);
+  assert.equal(0, numDownvotes);
+
+  I.click({css: '.voteBox:nth-of-type(1) form:nth-of-type(1) input[type="image"]'});
+
+  numUpvotes = await I.grabTextFrom('.voteBox:nth-of-type(1) span:nth-of-type(1)')
+  numDownvotes = await I.grabTextFrom('.voteBox:nth-of-type(1) span:nth-of-type(2)')
+
+  assert.equal(1, numUpvotes);
+  assert.equal(0, numDownvotes);
+
+  logoutPage.logout();
+
+  // Second user
+  registerPage.register();
+
+  I.click("Questions")
+  I.click(question.title);
+  I.click({css: '.voteBox:nth-of-type(1) form:nth-of-type(1) input[type="image"]'});
+
+  numUpvotes = await I.grabTextFrom('.voteBox:nth-of-type(1) span:nth-of-type(1)')
+  numDownvotes = await I.grabTextFrom('.voteBox:nth-of-type(1) span:nth-of-type(2)')
+
+  assert.equal(2, numUpvotes);
+  assert.equal(0, numDownvotes);
+
+  logoutPage.logout();
+
+  // Third user
+  registerPage.register();
+
+  I.click("Questions")
+  I.click(question.title);
+  I.click({css: '.voteBox:nth-of-type(1) form:nth-of-type(1) input[type="image"]'});
+
+  numUpvotes = await I.grabTextFrom('.voteBox:nth-of-type(1) span:nth-of-type(1)')
+  numDownvotes = await I.grabTextFrom('.voteBox:nth-of-type(1) span:nth-of-type(2)')
+
+  assert.equal(3, numUpvotes);
+  assert.equal(0, numDownvotes);
+});
