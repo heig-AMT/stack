dbdea1deb79618a21a145004ec6003de1068ff45
Alexandre Piveteau <alexandre@piveteau.email>
2020-10-26 23:29:14 +0100

    Add JdbcCommentRepository implementation

--- src/main/java/ch/heigvd/amt/stack/infrastructure/persistence/database/JdbcCommentRepository.java
+++ src/main/java/ch/heigvd/amt/stack/infrastructure/persistence/database/JdbcCommentRepository.java
@@ -1,6 +1,8 @@
 package ch.heigvd.amt.stack.infrastructure.persistence.database;
 
 import ch.heigvd.amt.stack.application.answer.query.CommentQuery;
+import ch.heigvd.amt.stack.domain.answer.AnswerId;
+import ch.heigvd.amt.stack.domain.authentication.CredentialId;
 import ch.heigvd.amt.stack.domain.comment.Comment;
 import ch.heigvd.amt.stack.domain.comment.CommentId;
 import ch.heigvd.amt.stack.domain.comment.CommentRepository;
@@ -9,9 +11,14 @@ import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @ApplicationScoped
@@ -35,21 +42,84 @@ public class JdbcCommentRepository extends JdbcRepository<Comment, CommentId> im
 
     @Override
     public void save(Comment comment) {
-
+        setup(dataSource);
+        var insert = "INSERT INTO Comment(idComment, idxCredential, idxAnswer, contents, instant) VALUES (?, ?, ?, ?, ?);";
+        try (var connection = getDataSource().getConnection()) {
+            var statement = connection.prepareStatement(insert);
+            statement.setString(1, comment.getId().toString());
+            statement.setString(2, comment.getCreator().toString());
+            statement.setString(3, comment.getAnswer().toString());
+            statement.setString(4, comment.getContents());
+            statement.setTimestamp(5, Timestamp.from(comment.getCreation()));
+            statement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger("JDBC").log(Level.WARNING, "Could not add comment " + comment.getId());
+        }
     }
 
     @Override
     public void remove(CommentId commentId) {
-
+        setup(dataSource);
+        var delete = "DELETE FROM Comment WHERE idComment = ?;";
+        try (var connection = getDataSource().getConnection()) {
+            var statement = connection.prepareStatement(delete);
+            statement.setString(1, commentId.toString());
+            statement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger("JDBC").log(Level.WARNING, "Could not remove comment " + commentId);
+        }
     }
 
     @Override
     public Optional<Comment> findById(CommentId commentId) {
+        setup(dataSource);
+        var select = "SELECT * FROM Comment WHERE idComment = ?;";
+        try (var connection = getDataSource().getConnection()) {
+            var statement = connection.prepareStatement(select);
+            statement.setString(1, commentId.toString());
+            var rs = statement.executeQuery();
+
+            if (rs.next()) {
+                return Optional.of(Comment.builder()
+                        .id(CommentId.from(rs.getString("idComment")))
+                        .answer(AnswerId.from(rs.getString("idxAnswer")))
+                        .creator(CredentialId.from(rs.getString("idxCredential")))
+                        .contents(rs.getString("contents"))
+                        .creation(rs.getTimestamp("instant").toInstant())
+                        .build());
+            } else {
+                return Optional.empty();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger("JDBC").log(Level.WARNING, "Could not find comment " + commentId);
+        }
         return Optional.empty();
     }
 
     @Override
     public Collection<Comment> findAll() {
-        return List.of();
+        setup(dataSource);
+        var select = "SELECT * FROM Comment;";
+        Collection<Comment> result = new ArrayList<>();
+        try (var connection = getDataSource().getConnection()) {
+            var statement = connection.prepareStatement(select);
+            var rs = statement.executeQuery();
+
+            while (rs.next()) {
+                Comment newC = Comment.builder()
+                        .id(CommentId.from(rs.getString("idComment")))
+                        .answer(AnswerId.from(rs.getString("idxAnswer")))
+                        .creator(CredentialId.from(rs.getString("idxCredential")))
+                        .contents(rs.getString("contents"))
+                        .creation(rs.getTimestamp("instant").toInstant())
+                        .build();
+                result.add(newC);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger("JDBC").log(Level.WARNING, "Could not findAll()");
+            return Collections.emptyList();
+        }
+        return result;
     }
 }
