173230c62d40a43468d71388d8b209dbea4e55bd
David <duprazdavid@gmail.com>
2020-10-28 21:53:26 +0100

    Second test added

--- src/test/java/ch/heigvd/amt/stack/application/AuthenticationFacadeIT.java
+++ src/test/java/ch/heigvd/amt/stack/application/AuthenticationFacadeIT.java
@@ -1,8 +1,11 @@
 package ch.heigvd.amt.stack.application;
 
 import ch.heigvd.amt.stack.application.authentication.command.LoginCommand;
+import ch.heigvd.amt.stack.application.authentication.command.LogoutCommand;
 import ch.heigvd.amt.stack.application.authentication.command.RegisterCommand;
+import ch.heigvd.amt.stack.application.authentication.dto.ConnectedDTO;
 import ch.heigvd.amt.stack.application.authentication.query.SessionQuery;
+import lombok.extern.java.Log;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -12,6 +15,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
+
+import static org.junit.Assert.assertThrows;
+//import static org.junit.Assert.assertTrue;
 
 @RunWith(Arquillian.class)
 public class AuthenticationFacadeIT {
@@ -51,4 +57,96 @@ public class AuthenticationFacadeIT {
 
         Assert.assertTrue(connected.isConnected());
     }
+
+    @Test
+    public void testTwoDifferentUsersCanSignUp() {
+        Assert.assertNotNull(authenticationFacade);
+
+        authenticationFacade.register(RegisterCommand.builder()
+                .username("aloy")
+                .password("chieftain")
+                .tag("scorcher")
+                .build());
+        ConnectedDTO connectedFirstUser = authenticationFacade.connected(SessionQuery.builder().tag("scorcher").build());
+
+        authenticationFacade.register(RegisterCommand.builder()
+                .username("ikrie")
+                .password("banuk")
+                .tag("scrapper")
+                .build());
+        ConnectedDTO connectedSecondUser = authenticationFacade.connected(SessionQuery.builder().tag("scrapper").build());
+
+        Assert.assertTrue(connectedFirstUser.isConnected());
+        Assert.assertTrue(connectedSecondUser.isConnected());
+    }
+
+    /*@Test
+    public void testOneUserCanNotSignUpTwoTimes() {
+
+
+        authenticationFacade.register(RegisterCommand.builder()
+                .username("aloy")
+                .password("chieftain")
+                .tag("scorcher")
+                .build());
+
+        assertThrows(AuthenticationFailedException.class, () -> {
+            authenticationFacade.register(RegisterCommand.builder()
+                    .username("aloy")
+                    .password("chieftain")
+                    .tag("scorcher")
+                    .build());
+        });
+
+    }
+
+    /*@Test
+    public void oneUserCanRegisterThenLogin() {
+        authenticationFacade.register(RegisterCommand.builder()
+                .username("aloy")
+                .password("chieftain")
+                .tag("scorcher")
+                .build());
+
+        authenticationFacade.login(LoginCommand.builder()
+                .username("aloy")
+                .password("chieftain")
+                .tag("scorcher")
+                .build());
+        ConnectedDTO connectedLogin = authenticationFacade.connected(SessionQuery.builder().tag("scorcher").build());
+
+        assertTrue(connectedLogin.isConnected());
+
+    }
+
+    @Test
+    public void oneUserWhoDoesNotExistCannotLogin() {
+
+        assertThrows(AuthenticationFailedException.class, () -> {
+            authenticationFacade.login(LoginCommand.builder()
+                    .username("aloy")
+                    .password("chieftain")
+                    .tag("scorcher")
+                    .build());
+        });
+
+    }
+
+    @Test
+    public void oneUserCanRegisterThenLogout() {
+
+
+        authenticationFacade.register(RegisterCommand.builder()
+                .username("aloy")
+                .password("chieftain")
+                .tag("scorcher")
+                .build());
+
+        authenticationFacade.logout(LogoutCommand.builder()
+                .tag("scorcher")
+                .build());
+        ConnectedDTO connectedLogin = authenticationFacade.connected(SessionQuery.builder().tag("scorcher").build());
+
+        assertTrue(connectedLogin.isConnected());
+    }*/
 }
