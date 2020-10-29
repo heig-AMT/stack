9db1c8556cf1e882f971e35321dbeb3b731f1e9a
Matthieu Burguburu <36692720+matt989253@users.noreply.github.com>
2020-10-29 23:16:17 +0100

    Update codecept.conf.js

--- tests/e2e/codecept.conf.js
+++ tests/e2e/codecept.conf.js
@@ -7,7 +7,7 @@ exports.config = {
   output: './output',
   helpers: {
     WebDriver: {
-      url: 'http://localhost:8090/mvc-simple',
+      url: 'http://localhost:8080',
       browser: 'chrome',
       host: '127.0.0.1',
       port: 4444,
