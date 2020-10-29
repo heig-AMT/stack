d503ea8292e941a7339a5fd358971f5fcef97294
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-10-29 23:00:25 +0100

    Use payara

--- docker/images/payara/entrypoint.sh
+++ docker/images/payara/entrypoint.sh
@@ -4,5 +4,6 @@ java                                                    \
     -XX:+UseContainerSupport                            \
     -XX:MaxRAMPercentage=90.0                           \
     -jar payara-micro.jar                               \
-    --deploy /opt/payara/deployments/mvc-simple.war:    \
-    --port $PORT
+    --deploy /opt/payara/deployments/mvc-simple.war    \
+    --port $PORT \
+    --addJars /opt/payara/deployments/database-connector.jar
