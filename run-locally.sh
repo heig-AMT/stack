4b51a8585ff99559c5718a10a72d7f9c35365bf3
Guy-Laurent Subri <guy-laurent@subri.ch>
2021-01-04 08:19:34 +0100

    Launch all instances in the same network

--- run-locally.sh
+++ run-locally.sh
@@ -19,22 +19,34 @@ getApiToken() {
     -X POST \
     -d "{ \"username\": \"$user\", \"password\": \"$pass\"}" \
     localhost:$port/account \
-    | jq '.token'
+    | jq -r '.token'
 }
 
 cd docker/topologies/local
 
+network="stack-net"
+
+gamifyOutsidePort=8081
+gamifyInsidePort=1234
+
+user="stack"
+pass="1234thisisapassword"
+
 # Be sure everything is down
+docker network rm $network
 docker-compose -f stack-topology.yml down
 docker-compose -f gamify-topology.yml down
+
+# Create external network
+docker network create $network
 
 # Run gamify server
 docker-compose -f gamify-topology.yml up --build --force-recreate --no-deps &
 
-waitOnPort 8081
+waitOnPort $gamifyOutsidePort
 
 # Run Stack Underflow
-GAMIFY_API_TOKEN=$(getApiToken 8081 "stack" "1234thisisapassword") \
-GAMIFY_SERVER="http://127.0.0.1:8081" \
+GAMIFY_API_TOKEN=$(getApiToken $gamifyOutsidePort $user $pass) \
+GAMIFY_SERVER="http://gamify:${gamifyInsidePort}" \
 sh -c 'docker-compose -f stack-topology.yml up --build --force-recreate --no-deps'
 
