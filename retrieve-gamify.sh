fd40867701097d8cfb8edc64dd6d03ccc55307ee
Guy-Laurent Subri <guy-laurent@subri.ch>
2021-01-05 17:20:50 +0100

    Removing obsolete helper script

--- retrieve-gamify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo $TOKEN_GITHUB | docker login https://docker.pkg.github.com --username heig-AMT-bot --password-stdin
-
-docker image pull docker.pkg.github.com/heig-amt/gamify/openjdk:latest
