1f0aeb9fc42d89aebb53c9454241a0897c6951da
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-21 18:02:02 +0100

    Ignore failed registration

--- src/test/java/ch/heigvd/amt/stack/application/AnswerFacadeIT.java
+++ src/test/java/ch/heigvd/amt/stack/application/AnswerFacadeIT.java
@@ -10,6 +10,7 @@ import ch.heigvd.amt.stack.application.question.command.DeleteQuestionCommand;
 import ch.heigvd.amt.stack.domain.answer.Answer;
 import ch.heigvd.amt.stack.domain.answer.AnswerId;
 import ch.heigvd.amt.stack.domain.answer.AnswerNotFoundException;
+import ch.heigvd.amt.stack.domain.authentication.AuthenticationFailedException;
 import ch.heigvd.amt.stack.domain.comment.CommentId;
 import ch.heigvd.amt.stack.domain.comment.CommentNotFoundException;
 import ch.heigvd.amt.stack.domain.question.QuestionNotFoundException;
@@ -44,6 +45,8 @@ public class AnswerFacadeIT {
     private void register() {
         try {
             unregister();
+        } catch (AuthenticationFailedException ignored) {
+            // Ignored.
         } finally {
             authenticationFacade.register(RegisterCommand.builder()
                 .username("ciri")
