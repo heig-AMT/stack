7721d00686f03ee867f96bb148561faf22745775
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-21 15:32:41 +0100

    Use GitHub Container Registry instead of GitHub Packages

--- publish-image-github.sh
+++ publish-image-github.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Tag the Docker images.
-docker tag stack-payara docker.pkg.github.com/heig-amt/stack/payara
-docker tag stack-openliberty docker.pkg.github.com/heig-amt/stack/openliberty
+docker tag stack-payara ghcr.io/heig-amt/stack-payara
+docker tag stack-openliberty ghcr.io/heig-amt/stack-openliberty
 
-# Log in to GitHub package registry.
-echo $TOKEN_GITHUB | docker login https://docker.pkg.github.com -u heig-AMT-bot --password-stdin
+# Log in to GitHub container registry.
+echo $TOKEN_GITHUB | docker login ghcr.io -u heig-AMT-bot --password-stdin
 
 # Push the Docker images.
-docker push docker.pkg.github.com/heig-amt/stack/payara
-docker push docker.pkg.github.com/heig-amt/stack/openliberty
+docker push ghcr.io/heig-amt/stack-payara
+docker push ghcr.io/heig-amt/stack-openliberty
